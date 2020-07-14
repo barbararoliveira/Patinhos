94dae1eee1c59ead3ca434c06ce5d7a417df38ad
Diego Neri <diego@diegoneri.com>
2020-07-14 11:06:55 -0300

    Corrige o programa -
    1 - Formata o documento com Alt + Shift + F; 2 - Corrige a linha 74 para exibir o total de patinhos

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Patinhos
 {
@@ -11,72 +11,71 @@ namespace Patinhos
             bool patovalido;
 
             Console.ForegroundColor = ConsoleColor.Blue;
-                    Console.WriteLine(" ----- CINCO PATINHOS ----- \n");
+            Console.WriteLine(" ----- CINCO PATINHOS ----- \n");
 
-            ConsoleColor padrao = Console.ForegroundColor; 
+            ConsoleColor padrao = Console.ForegroundColor;
 
             Console.ForegroundColor = ConsoleColor.White;
-                    Console.Write("Quantos patinhos (mínimo 2)? ");
-                            patovalido = Int32.TryParse (Console.ReadLine(), out patinhos);
+            Console.Write("Quantos patinhos (mínimo 2)? ");
+            patovalido = Int32.TryParse(Console.ReadLine(), out patinhos);
 
             if (!patovalido || patinhos < 2)
 
             {
-              Console.Write("Programa finalizado: número de patinhos \n");
-                      Console.ForegroundColor = ConsoleColor.DarkRed;
-                              Console.WriteLine("INVÁLIDO.");
-                                      Console.ForegroundColor = padrao;
-                                              Environment.Exit(-1);
+                Console.Write("Programa finalizado: número de patinhos \n");
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("INVÁLIDO.");
+                Console.ForegroundColor = padrao;
+                Environment.Exit(-1);
             }
 
             for (patofinal = patinhos; patofinal > 0; patofinal--)
 
             {
-                    if (patofinal > 1 )
+                if (patofinal > 1)
 
-                    {
+                {
                     Console.WriteLine($"\n{patofinal} patinhos foram passear");
-                    }
+                }
 
-                    else 
+                else
 
-                    {
+                {
                     Console.WriteLine($"\n{patofinal} patinho foi passear");
-                    }
+                }
 
-                    Console.WriteLine($"Além das montanhas");
-                    Console.WriteLine($"Para brincar");
-                    Console.WriteLine($"A mamãe gritou: Quá, quá, quá, quá");
+                Console.WriteLine($"Além das montanhas");
+                Console.WriteLine($"Para brincar");
+                Console.WriteLine($"A mamãe gritou: Quá, quá, quá, quá");
 
-                    if (patofinal >2)
+                if (patofinal > 2)
 
-                    {
+                {
                     Console.WriteLine($"Mas só {patofinal - 1} patinhos voltaram de lá.\n");
-                    }
+                }
 
-                    else if (patofinal ==1)
+                else if (patofinal == 1)
 
-                    {
-                    Console.WriteLine($"Mas nenhum patinho voltou de lá.\n");  
-                    }
+                {
+                    Console.WriteLine($"Mas nenhum patinho voltou de lá.\n");
+                }
 
-                    else
+                else
 
-                    {
+                {
                     Console.WriteLine($"Mas só {patofinal - 1} patinho voltou de lá.\n");
-                    }
+                }
 
             }
 
             Console.WriteLine("A mamãe patinha foi procurar");
             Console.WriteLine("Além das montanhas na beira do mar");
             Console.WriteLine("A mamãe gritou: Quá, quá, quá, quá");
-            Console.WriteLine($"E os 5 patinhos voltaram de lá\n");
- 
-                    Console.WriteLine("Pressione enter para finalizar <3 ");
-                            Console.ReadKey();
+            Console.WriteLine($"E os {patinhos} patinhos voltaram de lá\n");
 
+            Console.WriteLine("Pressione enter para finalizar <3 ");
+            Console.ReadKey();
+
+        }
     }
-  }
 }
-
